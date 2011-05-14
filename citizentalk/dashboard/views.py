c70c43ad662ba7e1184e642f5e2538d2cb7943a4
Andrei Savu <savu.andrei@gmail.com>
2011-05-14 14:26:15 +0300

    register request context and change sign in / sign out links

--- citizentalk/dashboard/views.py
+++ citizentalk/dashboard/views.py
@@ -1,6 +1,8 @@
 
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(request):
-    return render_to_response('dashboard/index.html')
+    return render_to_response('dashboard/index.html',
+        context_instance = RequestContext(request))
 
