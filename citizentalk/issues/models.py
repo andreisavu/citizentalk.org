a5c1b2ad5ce58eb3d138ff20b67b6059cb23c015
Andrei Bocan <zmaxor@gmail.com>
2011-05-14 16:10:17 +0300

    Tags can be set multiple times

--- citizentalk/issues/models.py
+++ citizentalk/issues/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import tagging
+from tagging.fields import TagField
 
 class Issue(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +9,7 @@ class Issue(models.Model):
     updated_at = models.DateTimeField('date updated', auto_now = True)
 
 tagging.register(Issue)
+
+# horrible hack for failing tags overwrite
+from tagging.models import Tag
+Tag.name_any = property(lambda self: self.name)
