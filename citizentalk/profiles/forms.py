89913e36621c3dad236fef4ef2fdd914ecd4a53c
Andrei Savu <savu.andrei@gmail.com>
2011-05-14 16:52:31 +0300

    created basic form for creating new accounts

--- citizentalk/profiles/forms.py
+++
@@ -1,7 +0,0 @@
-
-from django import forms
-
-class JoinForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField()
-
--- citizentalk/profiles/views.py
+++ citizentalk/profiles/views.py
@@ -1,12 +1,24 @@
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 
-from profiles.forms import JoinForm
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class JoinForm(UserCreationForm):
+    email = forms.EmailField(required = True)
 
 def join(request):
+    if request.method == 'POST':
+        form = JoinForm(request.POST)
+        if form.is_valid():
+            return redirect('/login')
+
+    else:
+        form =JoinForm()
 
     return render_to_response('profiles/join.html',
-        {'form': JoinForm()},
+        {'form': form},
         context_instance = RequestContext(request))
 
