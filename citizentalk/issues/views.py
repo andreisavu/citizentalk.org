632d85b88780ac3310e5f21dc9855fc7d8e68043
Andrei Savu <savu.andrei@gmail.com>
2011-05-14 14:13:26 +0300

    dummy page for exploring the number of issues

--- citizentalk/issues/views.py
+++ citizentalk/issues/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render_to_response
 def index(request):
     return render_to_response('issues/index.html')
 
+def explore(request):
+    return render_to_response('issues/explore.html')
+
