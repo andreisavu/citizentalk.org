0c98e8d2c731cb44f42251a5225b5b0e5fc77ad0
Andrei Bocan <zmaxor@gmail.com>
2011-05-14 16:54:17 +0300

    Add attachments

--- citizentalk/issues/models.py
+++ citizentalk/issues/models.py
@@ -2,6 +2,10 @@ from django.db import models
 import tagging
 from tagging.fields import TagField
 
+class Attachment(models.Model):
+    file = models.FileField(upload_to='file/%Y/%m/%d')
+    name = models.CharField(max_length=100)
+
 class Issue(models.Model):
     ISSUE_STATES = (
         (u'new', u'New'),
@@ -12,6 +16,7 @@ class Issue(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     state = models.CharField(max_length=3, choices=ISSUE_STATES)
+    attachments = models.ManyToManyField(Attachment)
     created_at = models.DateTimeField('date created', auto_now_add = True)
     updated_at = models.DateTimeField('date updated', auto_now = True)
 
