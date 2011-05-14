616d3dff52da71243a007f0f35ad9e4dc82a3e55
Andrei Savu <savu.andrei@gmail.com>
2011-05-14 10:29:05 +0300

    added config options for the django debug toolbar

--- citizentalk/settings.py
+++ citizentalk/settings.py
@@ -107,6 +107,12 @@ MIDDLEWARE_CLASSES = (
 
 INTERNAL_IPS = ('127.0.0.1',)
 
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'HIDE_DJANGO_SQL': False,
+    'TAG': 'div',
+}
+
 ROOT_URLCONF = 'citizentalk.urls'
 
 TEMPLATE_DIRS = (
