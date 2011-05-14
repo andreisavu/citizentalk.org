8f64c7bf75c42eb28a6ceae6556b426487b2d6b3
Andrei Bocan <zmaxor@gmail.com>
2011-05-14 21:24:27 +0300

    Adding an issue kind of sort of

--- citizentalk/issues/views.py
+++ citizentalk/issues/views.py
@@ -1,5 +1,4 @@
-
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.http import Http404
 
@@ -10,6 +9,32 @@ from issues.models import Issue
 from issues.forms import CreateForm
 
 def index(request):
+    if request.method == 'GET':
+        return list_issues(request)
+    elif request.method == 'POST':
+        return create(request)
+
+@login_required
+def new(request):
+    form = CreateForm()
+    return render_to_response('issues/new.html',
+        {'form': form},
+        context_instance = RequestContext(request))
+
+@login_required
+def create(request):
+    form = CreateForm(request.POST)
+
+    if form.is_valid():
+        issue = create_issue(request)
+        issue.save()
+        return redirect('/issues/')
+    else:
+        return render_to_response('issues/new.html',
+            {'form': form},
+            context_instance = RequestContext(request))
+
+def list_issues(request):
     try:
         page = int(request.GET.get('page','1'))
     except (ValueError, TypeError):
@@ -26,10 +51,10 @@ def index(request):
         {'paginator': paginator, 'issues': issues},
         context_instance = RequestContext(request))
 
-@login_required
-def new(request):
-    form = CreateForm()
-    return render_to_response('issues/new.html',
-        {'form': form},
-        context_instance = RequestContext(request))
-
+def create_issue(request):
+    return Issue(title = request.POST['title'],
+        description = request.POST['description'],
+        latitude = request.POST['latitude'],
+        longitude = request.POST['longitude'],
+        tags = request.POST['tags'],
+        reporter = request.user)
