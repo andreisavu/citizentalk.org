a50c4a15edf60124949f8ed04054f19d2b2d0511
Andrei Savu <savu.andrei@gmail.com>
2011-05-14 12:26:23 +0300

    added layout elements

--- citizentalk/settings.py
+++ citizentalk/settings.py
@@ -110,7 +110,6 @@ INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
     'HIDE_DJANGO_SQL': False,
-    'TAG': 'div',
 }
 
 ROOT_URLCONF = 'citizentalk.urls'
