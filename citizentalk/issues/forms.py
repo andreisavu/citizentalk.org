3f980215faba44a3c4f7e23955875a3176234a33
Andrei Savu <savu.andrei@gmail.com>
2011-05-15 09:59:09 +0300

    issue add form ready for rock & roll

--- citizentalk/issues/forms.py
+++ citizentalk/issues/forms.py
@@ -5,7 +5,7 @@ from issues.models import Issue
 class CreateForm(forms.Form):
     title = forms.CharField(required=True)
     description = forms.CharField(widget=forms.Textarea)
-    tags = forms.CharField()
+    tags = forms.CharField(required=False)
     latitude = forms.DecimalField(widget=forms.HiddenInput)
     longitude = forms.DecimalField(widget=forms.HiddenInput)
 
@@ -13,6 +13,6 @@ class CreateForm(forms.Form):
         cleaned_data = self.cleaned_data
         tags = cleaned_data.get("tags")
 
-        if u':' in tags:
+        if tags and u':' in tags:
             raise forms.ValidationError("Colons are not allowed in the "
                                     "tags field")
