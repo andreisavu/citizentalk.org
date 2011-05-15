4d0eb87784ab617851e6b18739286d2e4de5afe3
Andrei Savu <savu.andrei@gmail.com>
2011-05-15 09:55:00 +0300

    added basic counters for homepage

--- citizentalk/dashboard/views.py
+++ citizentalk/dashboard/views.py
@@ -1,8 +1,18 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.db.models import Q
+
+from issues.models import Issue
 
 def index(request):
+    reported_issues = Issue.objects.all().count()
+    fixed_issues = Issue.objects\
+        .filter(Q(state = u'res') | Q(state = u'acc')).count()
+    updates_on_reports = 0
+
     return render_to_response('dashboard/index.html',
+        {'reported_issues': reported_issues, 'fixed_issues': fixed_issues,
+         'updates_on_reports': updates_on_reports},
         context_instance = RequestContext(request))
 
